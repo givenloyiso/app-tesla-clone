82280fea7d5d112bcc5cbb91c29fbee9c4dd741b
Given Loyiso <info@givenloyiso.com>
2021-11-14 16:42:27 +0100

    Done with Tesla Clone APP

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -2,15 +2,23 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
-		position: absolute,
+		position: 'absolute',
+		top: 50,
+		zIndex: 100,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: '100%',
+		paddingHorizontal: 20
 	},
 	logo: {
-		width: '100',
-		height: '20',
+		width: 100,
+		height: 20,
+		resizeMode: 'contain'
 	},
 	menu: {
-		width: '25',
-		height: '25',
+		width: 25,
+		height: 25,
+		resizeMode: 'contain'
 	},
 });
 
